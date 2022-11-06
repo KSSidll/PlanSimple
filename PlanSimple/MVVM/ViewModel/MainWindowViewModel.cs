2e2708aeea16b08b5ec0bf0c0ff566ad5907641a
fifi000 <filipkociok@gmail.com>
2022-11-06 16:59:59 +0100

    Add CalendarView and ViewModel to NavBar

--- PlanSimple/MVVM/ViewModel/MainWindowViewModel.cs
+++ PlanSimple/MVVM/ViewModel/MainWindowViewModel.cs
@@ -10,18 +10,22 @@ public class MainWindowViewModel : ObservableObject
 	{
 		HomeViewModel = new HomeViewModel();
 		NotesViewModel = new NotesViewModel();
+        CalendarViewModel = new CalendarViewModel();
 
 		CurrentView = HomeViewModel;
 
 		HomeViewCommand = new RelayCommand(o => { CurrentView = HomeViewModel; });
 		NotesViewCommand = new RelayCommand(o => { CurrentView = NotesViewModel; });
+        CalendarViewCommand = new RelayCommand(o => { CurrentView = CalendarViewModel; });
 	}
 
 	public RelayCommand HomeViewCommand { get; }
 	public RelayCommand NotesViewCommand { get; }
+	public RelayCommand CalendarViewCommand { get; }
 
 	public HomeViewModel HomeViewModel { get; }
 	public NotesViewModel NotesViewModel { get; }
+	public CalendarViewModel CalendarViewModel { get; }
 
 	public object? CurrentView
 	{
