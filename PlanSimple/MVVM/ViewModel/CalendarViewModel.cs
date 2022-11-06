1b30d820b3ab264be8df47ce98b487453c0b9e56
fifi000 <filipkociok@gmail.com>
2022-11-06 17:46:15 +0100

    Add row with week days

--- PlanSimple/MVVM/ViewModel/CalendarViewModel.cs
+++ PlanSimple/MVVM/ViewModel/CalendarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlanSimple.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace PlanSimple.MVVM.ViewModel
 {
-    public class CalendarViewModel
+    public class WeekDayModel
     {
+        public string DayName { get; set; }
+        public int DayNumber { get; set; }
+    }
+
+    public class Day
+
+    public class CalendarViewModel
+    {       
+        public List<WeekDayModel> WeekDays { get; set; } = new List<WeekDayModel>();
+        public CalendarViewModel()
+        {
+            SetTestData();
+        }
+
+        private void SetTestData()
+        {
+            WeekDays = new List<WeekDayModel>
+            {
+               new WeekDayModel{ DayName="Mon", DayNumber=25 },
+               new WeekDayModel{ DayName="Tues", DayNumber=26 },
+               new WeekDayModel{ DayName="Wed", DayNumber=27 },
+               new WeekDayModel{ DayName="Thurs", DayNumber=28 },
+               new WeekDayModel{ DayName="Fri", DayNumber=29 },
+               new WeekDayModel{ DayName="Sat", DayNumber=30 },
+               new WeekDayModel{ DayName="Sun", DayNumber=1 }
+            };
+        }
     }
 }
