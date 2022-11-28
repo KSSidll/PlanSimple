e024ed0f78580bd3d4d6e34ba31a3aeed1d7d380
fifi000 <filipkociok@gmail.com>
2022-11-28 23:46:35 +0100

    Add Functionality to Previous and Next Week
    Next and Prev buttons change weeks
    and updates showed notes.
    Updates WeekDayModel.
    Adds CalendarHelper static class.

---
+++ PlanSimple/HelperClasses/CalendarHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanSimple.HelperClasses
+{
+    public static class CalendarHelper
+    {
+        public static readonly Dictionary<string, string> WeekDaysShortNames = new Dictionary<string, string>
+        {
+            {"Monday", "Mon"},
+            {"Tuesday", "Tues"},
+            {"Wednesday", "Wed"},
+            {"Thursday", "Thurs"},
+            {"Friday", "Fri"},
+            {"Saturday", "Sat"},
+            {"Sunday", "Sun"},
+        };
+        public static List<DateTime> GetWeek(DateTime day)
+        {
+            List<DateTime> week = new List<DateTime>();
+
+            if (day.DayOfWeek == DayOfWeek.Sunday)
+            {
+                for (int i = 6; i >= 0; i--)
+                {
+                    week.Add(day.AddDays(-i));
+                }
+            }
+            else
+            {
+                var x = (int)day.DayOfWeek - (int)DayOfWeek.Monday;
+
+                // before
+                for (int i = x; i > (int)day.DayOfWeek; i--)
+                {
+                    week.Add(day.AddDays(-i));
+                }
+                // after
+                for (int i = 0; i <= (int)DayOfWeek.Saturday; i++)
+                {
+                    week.Add(day.AddDays(i));
+                }
+            }
+
+            return week;
+        }
+    }
+}
--- PlanSimple/MVVM/ViewModel/CalendarViewModel.cs
+++ PlanSimple/MVVM/ViewModel/CalendarViewModel.cs
@@ -1,39 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
+using System.Windows;
 using Microsoft.EntityFrameworkCore;
+using PlanSimple.Core;
 using PlanSimple.Database.Context;
 using PlanSimple.Database.Model;
 using PlanSimple.MVVM.Model;
+using PlanSimple.HelperClasses;
 
 namespace PlanSimple.MVVM.ViewModel
 {
 
     public class CalendarViewModel : BaseViewModel
     {       
-        public List<WeekDayModel> Week { get; set; } = new();
+        public BindingList<WeekDayModel> Week { get; set; } = new();
         public List<ToDoListDisplayModel> Days { get; set; } = new();
-        
+
+        public RelayCommand? PreviousWeek { get; }
+        public RelayCommand? NextWeek { get; }
+
         private static readonly ToDoNoteContext ToDoNoteContext = new();
         private DbSet<ToDoNote> ToDoNotes => ToDoNoteContext.ToDoNotes;
         
         public CalendarViewModel()
         {
-            SetTestData();
+            SetWeekDays();
+
+            GetNotesForWeek();
+
+            PreviousWeek = new RelayCommand(_ =>
+            {
+                for (int i = 0; i < Week.Count; i++)
+                {
+                    Week[i].Date = Week[i].Date.AddDays(7);
+                }
+                Week.ResetBindings();
+            });
+
+            NextWeek = new RelayCommand(_ =>
+            {
+                for (int i = 0; i < Week.Count; i++)
+                {
+                    Week[i].Date = Week[i].Date.AddDays(7);
+                }
+                Week.ResetBindings();
+            });
         }
+
+        private void SetWeekDays()
+        {
+            var currentDay = DateTime.Now;
+
+            var week = CalendarHelper.GetWeek(currentDay)
+                .Select(x => new WeekDayModel { Date = DateOnly.FromDateTime(x) })
+                .ToList();
+
+            Week = new BindingList<WeekDayModel>();
+
+            foreach (var item in week)
+            {
+                Week.Add(item);
+            }
+        }
+
+        private void GetNotesForWeek()
+        {
+            // filter out ToDoNotes that don't have any set date before grouping them into days 
+            var groups = ToDoNotes
+                .Where(x => x.Date != null)
+                .Where(x => Week.First().Date <= x.Date && x.Date <= Week.Last().Date)
+                .GroupBy(x => x.Date);
+
+            Days = new();
+
+            foreach (var g in groups)
+            {
+                ToDoListDisplayModel model = new ToDoListDisplayModel { Date = (DateOnly)g.Key! };
+                foreach (var element in g)
+                {
+                    model.ToDoNotes.Add(element);
+                }
+
+                Days.Add(model);
+            }
+        }
+
+
         
         private void SetTestData()
         {
-            Week = new List<WeekDayModel>
-            {
-               new WeekDayModel{ DayName="Mon", DayNumber=31 },
-               new WeekDayModel{ DayName="Tues", DayNumber=1 },
-               new WeekDayModel{ DayName="Wed", DayNumber=2 },
-               new WeekDayModel{ DayName="Thurs", DayNumber=3 },
-               new WeekDayModel{ DayName="Fri", DayNumber=4 },
-               new WeekDayModel{ DayName="Sat", DayNumber=5 },
-               new WeekDayModel{ DayName="Sun", DayNumber=6 }
-            };
 
             // If Note database is empty, add dummy data for test purposes
             if (!ToDoNotes.Any())
@@ -72,21 +130,8 @@ namespace PlanSimple.MVVM.ViewModel
                 });
                 
                 ToDoNoteContext.SaveChanges();
-            }
-            
-            // filter out ToDoNotes that don't have any set date before grouping them into days 
-            var groups = ToDoNotes.Where(x => x.Date != null).GroupBy(x => x.Date);
-        
-            foreach (var g in groups)
-            {
-                ToDoListDisplayModel model = new ToDoListDisplayModel { Date = (DateOnly) g.Key! };
-                foreach (var element in g)
-                {
-                    model.ToDoNotes.Add(element);
-                }
-        
-                Days.Add(model);
-            }
+            }         
+
         }
     }
 }
