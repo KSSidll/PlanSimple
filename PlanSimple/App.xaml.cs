2932bf6a4fdfc7db3bc4ee3a03b97eb158b8e161
KSSidll <kolasasidll@gmail.com>
2022-12-09 22:10:25 +0100

    Implement Light Theme.
    Signed-off-by: KSSidll <kssidll@users.noreply.github.com>

--- PlanSimple/App.xaml.cs
+++ PlanSimple/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using PlanSimple.Database.Context;
 
@@ -9,6 +10,12 @@ namespace PlanSimple
 	/// </summary>
 	public partial class App : Application
 	{
+		public void ChangeTheme(Uri uri)
+		{
+			Resources.MergedDictionaries[0].MergedDictionaries.Clear();
+			Resources.MergedDictionaries[0].MergedDictionaries.Add(new ResourceDictionary {Source = uri});
+		}
+		
 		protected override void OnStartup(StartupEventArgs e)
 		{
 			base.OnStartup(e);
--- PlanSimple/MVVM/Model/Enums.cs
+++ PlanSimple/MVVM/Model/Enums.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PlanSimple.MVVM.Model
+﻿namespace PlanSimple.MVVM.Model
 {
     public enum Priority
     {
         Low, Medium, High
+    }
+
+    public enum Theme
+    {
+        Dark, Light
     }
 }
